5245814ce3f2ddb5a9c0f32077eea12bb335c9be
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-29 17:02:57 +0200

    Modify preparation and cooking time validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,8 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
-  validates :preparation_time, presence: true, numericality: { greater_than: 0 }
-  validates :cooking_time, presence: true, numericality: { greater_than: 0 }
+  validates :preparation_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :cooking_time, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :public, inclusion: { in: [true, false] }
 
   belongs_to :user
