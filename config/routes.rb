5ed9de27e60eb4a4c46c67771080f7743ff030fa
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-29 17:10:21 +0200

    Add new and destroy action on recipes resourceful route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
-    resources :recipes, only: [:index, :show, :create]
+    resources :recipes, only: [:index, :new, :show, :create, :destroy]
   end
 end
