0ff30d8b595b98c544df390de0abc0bcec0c5350
Karan Joshi <Karan190213@gmail.com>
2024-01-29 19:52:44 +0530

    create public recipes

---
+++ app/controllers/public_recipes_controller.rb
@@ -0,0 +1,26 @@
+# app/controllers/public_recipes_controller.rb
+class PublicRecipesController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @public_recipes = Recipe.where(public: true).order(created_at: :desc)
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    if current_user == @recipe.user
+      @recipe.destroy
+      redirect_to public_recipes_path, notice: "Recipe deleted successfully."
+    else
+      redirect_to public_recipes_path, alert: "You are not authorized to delete this recipe."
+    end
+  end
+
+  def index
+    @public_recipes = Recipe.where(public: true).order(created_at: :desc).paginate(page: params[:page], per_page: 10)
+  end
+end
