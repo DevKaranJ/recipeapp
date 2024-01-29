afa1ec53de16828a61f7fa124de1793c29ff0135
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-29 16:42:48 +0200

    Add recipes validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :preparation_time, presence: true, numericality: { greater_than: 0 }
+  validates :cooking_time, presence: true, numericality: { greater_than: 0 }
+  validates :public, inclusion: { in: [true, false] }
 end
