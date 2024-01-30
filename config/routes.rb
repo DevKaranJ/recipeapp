f2ea64fb7e05b09f1110e95870d634b30a9f2958
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-30 09:19:29 +0200

    Add resourceful routing for foods controller with only index, new, create and destroy function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show] 
+  resources :users, only: [:index, :show]
+  resources :foods, only: [:index, :new, :create, :destroy] 
   resources :recipes, only: [:index, :new, :show, :create, :destroy]
 end
