bcc6efcce235134fdf895d38e40468a9962fc1e8
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-30 17:28:31 +0200

    Create nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "users#index"
   devise_for :users
-  resources :users, only: [:index, :show] 
-  resources :foods, only: [:index, :new, :create, :destroy] 
-  resources :recipes, only: [:index, :new, :show, :create, :destroy]
-  
+  resources :users, only: [:index, :show] do
+    resources :foods, only: [:index, :new, :create, :destroy] 
+    resources :recipes, only: [:index, :new, :show, :create, :destroy]
+  end
 end
