da70136bd49a1b23a793aa9f68f98bf46ed8c198
tsheporamantso <tshepo.ramantso@outlook.com>
2024-01-31 12:54:43 +0200

    Modify food controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "users#index"
   devise_for :users
   resources :users, only: [:index, :show] do
-    resources :foods, only: [:index, :new, :create, :destroy] 
+    resources :foods, only: [:index, :new, :create, :show, :destroy] 
     resources :recipes, only: [:index, :new, :show, :create, :destroy]
   end
 end
