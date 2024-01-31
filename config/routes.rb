80061f8246d84247f19c86b26583776a3d83c4a7
Karan Joshi <Karan190213@gmail.com>
2024-01-31 17:06:41 +0530

    add the ingredient page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
+  get 'ingredients/new'
+  get 'ingredients/create'
+  get 'ingredients/edit'
+  get 'ingredients/update'
+  get 'ingredients/destroy'
 
-  get "up" => "rails/health#show", as: :rails_health_check # health check
-
-  # root "posts#index"
-  root to: "users#index"
   devise_for :users
   resources :users, only: [:index, :show]
   resources :foods, only: [:index, :new, :create, :destroy]
-  resources :recipes, only: [:index, :new, :show, :create, :destroy]
+  resources :recipes, only: [:index, :new, :show, :create, :destroy] do
+    resources :ingredients, only: [:new, :create, :edit, :update, :destroy]
+  end
   resources :public_recipes, only: [:index, :show, :destroy]
   resources :general_shopping_lists, only: [:index]
 
