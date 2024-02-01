3de5f8248ed25818bd5ed5311351760b0d00546e
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-01 09:01:29 +0200

    Modify routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root to: "users#index"
   devise_for :users
+  root "users#index"
   resources :users, only: [:index, :show] do
     resources :foods, only: [:index, :new, :create, :show, :destroy] 
     resources :recipes, only: [:index, :new, :show, :create, :destroy]
