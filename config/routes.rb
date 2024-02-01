a6c092a11e699018ef11a794838d488b1933608f
tsheporamantso <tshepo.ramantso@outlook.com>
2024-02-01 11:05:08 +0200

    Nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
   get 'ingredients/destroy'
 
   devise_for :users
-  resources :users, only: [:index, :show]
-  resources :foods, only: [:index, :new, :create, :destroy]
+  root 'users#index'
+  resources :users, only: [:index, :show] do
+   resources :foods, only: [:index, :new, :create, :destroy]
   resources :recipes, only: [:index, :new, :show, :create, :destroy] do
     resources :ingredients, only: [:new, :create, :edit, :update, :destroy]
+   end
   end
   resources :public_recipes, only: [:index, :show, :destroy]
   resources :general_shopping_lists, only: [:index]
