3cb1cd59813644b8fda91e1fb9dc549057eb0ef8
Karan Joshi <Karan190213@gmail.com>
2024-02-02 16:33:46 +0530

    updated the general shopping list

--- app/controllers/general_shopping_lists_controller.rb
+++ app/controllers/general_shopping_lists_controller.rb
@@ -1,11 +1,32 @@
-# app/controllers/general_shopping_lists_controller.rb
-
 class GeneralShoppingListsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @missing_foods = current_user.calculate_missing_foods
-    @total_items = @missing_foods.sum(:quantity)
-    @total_price = @missing_foods.sum(:price)
+    @user = current_user
+    @recipes = Recipe.where(user_id: @user.id)
+    @missing_foods = calculate_missing_foods(@user, @recipes)
+    @total_items = @missing_foods.sum { |food_name, missing_quantity| missing_quantity }
+
+    # Calculate total price, handling nil case gracefully
+    @total_price = @missing_foods.sum do |food_name, missing_quantity|
+      food = Food.find_by(name: food_name)
+      food ? missing_quantity * food.price : 0
+    end
+  end
+
+  private
+
+  def calculate_missing_foods(user, recipes)
+    missing_foods = {}
+    ingredients = Ingredient.includes(:food).where(recipe_id: recipes.pluck(:id)).group_by { |ingredient| ingredient.food&.name }
+
+    ingredients.each do |food_name, ingredients|
+      total_quantity_required = ingredients.sum { |ingredient| ingredient.quantity.to_i }
+      user_food = user.foods.find_by(name: food_name)
+      missing_quantity = user_food ? [total_quantity_required - user_food.quantity, 0].max : total_quantity_required
+      missing_foods[food_name] = missing_quantity
+    end
+
+    missing_foods
   end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,6 @@
 class Ingredient < ApplicationRecord
   belongs_to :recipe
+  belongs_to :food
   validates :food_name, presence: true
   validates :quantity, presence: true
 end
